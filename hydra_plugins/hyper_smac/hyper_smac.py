75d4b8c11286fd04e45d632fafeeda4b8fdd39c2
benjamc <c.benjamins@ai.uni-hannover.de>
2024-05-16 15:05:44 +0200

    feat: add func to read configurations from file to be passed as additionalconfigs for warmstarting

--- hydra_plugins/hyper_smac/hyper_smac.py
+++ hydra_plugins/hyper_smac/hyper_smac.py
@@ -3,15 +3,96 @@
 from __future__ import annotations
 
 from pathlib import Path
+from typing import Any
 
+import ConfigSpace as CS  # noqa: N817
+import numpy as np
+import pandas as pd
+from ConfigSpace import Configuration, ConfigurationSpace
 from hydra.utils import get_class
-from omegaconf import OmegaConf
+from hydra_plugins.hypersweeper import Info
+from hydra_plugins.hypersweeper.search_space_encoding import (
+    search_space_to_config_space,
+)
+from omegaconf import DictConfig, OmegaConf
 from smac import Scenario
 from smac.runhistory.dataclasses import TrialInfo, TrialValue
 
-from hydra_plugins.hypersweeper import Info
+
+def maybe_convert_types(k: str, v: Any, configspace: ConfigurationSpace) -> Any | None:
+    """Maybe convert types to match conditions and HP types.
+
+    Types come from pandas Dataframe.
+
+    Parameters
+    ----------
+    k : str
+        Hyperparameter name
+    v : Any
+        Hyperparameter value as read in by pandas from csv
+    configspace : ConfigurationSpace
+        Configuration space with information about HP type
+
+    Returns.
+    --------
+    Any | None
+        The hyperparameter value with correct type
+    """
+    if np.isnan(v):
+        v = None
+    elif isinstance(v, float) and \
+        isinstance(
+            configspace[k],
+            CS.UniformIntegerHyperparameter | CS.NormalIntegerHyperparameter | CS.BetaIntegerHyperparameter
+        ):
+        v = int(v)
+    return v
+
+def convert_to_configuration(x: pd.Series, configspace: ConfigurationSpace) -> Configuration:
+    """Convert configurations from run logs (csv) to ConfigSpace.Configuration.
+
+    Parameters
+    ----------
+    x : pd.Series
+        Row from runlogs.
+    configspace : ConfigurationSpace
+        Configuration space, necessary for conversion.
+
+    Returns.
+    --------
+    Configuration
+        The converted configuration.
+    """
+    x = dict(x)
+    hp_config = {
+        k: maybe_convert_types(k, v, configspace=configspace) for k,v in x.items() if k.startswith("hp_config")
+    }
+    return Configuration(configuration_space=configspace, values=hp_config)
+
+
+def read_additional_configs(initial_design_fn: str, search_space: DictConfig = None) -> list[Configuration]:
+    """Read configurations from csv-logfile.
+
+    Parameters
+    ----------
+    initial_design_fn : str, optional
+        The path to the log file.
+    search_space : DictConfig, optional
+        The search space which will be converted to a ConfigSpace.ConfigurationSpace, by default None.
+        The search space can be loaded via `search_space = OmegaConf.load(search_space_fn)`.
+
+    Returns.
+    --------
+    list[Configuration]
+        The configurations from the log file.
+    """
+    configspace = search_space_to_config_space(search_space=search_space)
+    initial_design = pd.read_csv(initial_design_fn)
+    return initial_design.apply(convert_to_configuration, args=(configspace,), axis=1).to_list()
+
 
 OmegaConf.register_new_resolver("get_class", get_class, replace=True)
+OmegaConf.register_new_resolver("read_additional_configs", read_additional_configs, replace=True)
 
 
 class HyperSMACAdapter:
@@ -77,3 +158,6 @@ def make_smac(configspace, smac_args):
 
     smac = smac_args["smac_facade"](scenario, dummy_func, **smac_kwargs)
     return HyperSMACAdapter(smac)
+
+if __name__ == "__main__":
+    read_additional_configs()
