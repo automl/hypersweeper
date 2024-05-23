54787e2c788f4a75f97cbf1b377efabba55f7bfc
benjamc <c.benjamins@ai.uni-hannover.de>
2024-05-23 10:09:43 +0200

    refactor: import Result

--- hydra_plugins/hypersweeper/__init__.py
+++ hydra_plugins/hypersweeper/__init__.py
@@ -1,5 +1,5 @@
 from .hypersweeper import Hypersweeper
 from .hypersweeper_backend import HypersweeperBackend
-from .hypersweeper_sweeper import HypersweeperSweeper, Info
+from .hypersweeper_sweeper import HypersweeperSweeper, Info, Result
 
-__all__ = ["HypersweeperSweeper", "HypersweeperBackend", "Info", "Hypersweeper"]
+__all__ = ["HypersweeperSweeper", "HypersweeperBackend", "Info", "Result", "Hypersweeper"]
